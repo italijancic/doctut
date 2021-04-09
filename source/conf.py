3dd6d2b4f6430a39b45838a3a585a75f86b68018
italijancic <italijancic@outlook.com>
2021-04-09 02:13:45 +0100

    Reconfig html theme

--- source/conf.py
+++ source/conf.py
@@ -45,8 +45,6 @@ extensions = [
     "sphinx_rtd_theme",
 ]
 
-html_theme = "sphinx_rtd_theme"
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['ntemplates']
 
@@ -80,7 +78,9 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+
+# html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
